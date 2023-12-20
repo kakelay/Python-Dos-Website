138967dacdf248aab78ef5c4d719b1e5c98af731
kakelay <kakelay18052002@gmail.com>
2023-12-21 00:47:00 +0700

    first commit

---
+++ dos.py
@@ -0,0 +1,20 @@
+import requests
+
+# Replace this URL with the website you want to request
+url = 'https://portfolio-gd5c6tbhj-kruytharin.vercel.app/'
+
+# Number of requests
+num_requests = 1000
+
+for _ in range(num_requests):
+    try:
+        response = requests.get(url)
+        # You can add more code here to process the response, if needed
+        if response.status_code == 200:
+            print(f"Request successful: {response.status_code}")
+        else:
+            print(f"Request failed: {response.status_code}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+print("All requests completed.")
